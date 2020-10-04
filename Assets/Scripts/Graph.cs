7356ef3b710d3f13b1b6082c4c57960369582afe
Himanshu Gupta <thinktank.himanshu@gmail.com>
2020-10-04 14:36:24 +0530

    section 2: till lesson 10

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    // 2 dimentional array of nodes
+    public Node[,] nodes;
+    public List<Node> walls = new List<Node>();
+
+    int[,] _mapData;
+    int _width;
+    int _height;
+
+    public static readonly Vector2[] allDirections =
+    {
+        new Vector2(0f, 1f),
+        new Vector2(1f, 1f),
+        new Vector2(1f, 0f),
+        new Vector2(1f,-1f),
+        new Vector2(0, -1f),
+        new Vector2(-1f, -1f),
+        new Vector2(-1f, 0f),
+        new Vector2(-1f, 1f)
+    };
+
+    public void Init(int[,] mapData)
+    {
+        _mapData = mapData;
+        _width = mapData.GetLength(0);
+        _height = mapData.GetLength(1);
+
+        nodes = new Node[_width, _height];
+
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                NodeType type = (NodeType) mapData[x, y];
+                Node newNode = new Node(x, y, type);
+                nodes[x, y] = newNode;
+
+                newNode.position = new Vector3(x, 0, y);
+
+                if (type == NodeType.Blocked)
+                {
+                    walls.Add(newNode);
+                }
+            }
+        }
+
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                if (nodes[x, y].nodeType != NodeType.Blocked)
+                {
+                    nodes[x, y].neighbors = GetNeighbours(x, y);
+                }
+            }
+        }
+    }
+
+    public bool IsWithinBounds(int x, int y)
+    {
+        return (x >= 0 && x < _width && y >= 0 && y < _height);
+    }
+
+    List<Node> GetNeighbours(int x, int y, Node[,] nodeArray, Vector2[] directions)
+    {
+        List<Node> neighbourNodes = new List<Node>();
+
+        foreach (var dir in directions)
+        {
+            int newX = x + (int)dir.x;
+            int newY = y + (int)dir.y;
+
+            if (IsWithinBounds(newX, newY) && nodeArray[newX, newY] != null && nodeArray[newX, newY].nodeType != NodeType.Blocked)
+            {
+                neighbourNodes.Add(nodeArray[newX, newY]);
+            }
+        }
+
+        return neighbourNodes;
+    }
+
+    List<Node> GetNeighbours(int x, int y)
+    {
+        return GetNeighbours(x, y, nodes, allDirections);
+    }
+
+
+}
